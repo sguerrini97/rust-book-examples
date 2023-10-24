1d7aad9a6a26ab4f0561af41a1d0be11665c8435
Samuele Guerrini <sguerrini97@gmail.com>
2023-10-24 23:52:20 +0200

    Comments and minor change

--- 003_guessing_game/src/main.rs
+++ 003_guessing_game/src/main.rs
@@ -3,10 +3,12 @@ use std::io::{self, Write};
 fn main() {
     println!("Guess the number!");
 
+    // If we use print! instead of println!, the cursor stays on the same line ...
     print!("Please input your guess: ");
+    // ... but we have to flush the buffer manually to ensure the output is displayed immediately.
     io::stdout().flush().expect("Failed to flush");
 
-    let mut guess = String::new();
+    let mut guess = String::with_capacity(6);
 
     io::stdin()
         .read_line(&mut guess)
